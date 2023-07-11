c2341a4d3eb258f86f202755e34954aed3950675
Nicholas Feinberg <pleasingfung@gmail.com>
2023-07-11 14:02:40 -0700

    New talisman: Flux
    Intended to serve as a further goal for Shapeshifters to skill
    toward, flux talismans are a bridge between Beast form and later
    forms.
    
    Users inflict magical contamination on foes with each hit. Level 1
    contam backlights foes, revealing invisible enemies and reducing
    their EV. Level 3 causes a meltdown, doing high damage (0-30 at
    min Shapeshifting skill, 0-60 at max), malmutating the victim, and
    resetting their glow. This is intended to create a distinctive
    rhythm (1-2-3 hits) without breaking autofight or otherwise being
    'fiddly'.
    
    The tradeoff is that your unstable, flux-ridden form is unable to
    wield weapons or shields and just sometimes destabilizes mid-swing.
    The latter reduces post-AC melee damage by 1/3rd, similar to statue
    form's +50% damage. This is intended to give the contam part of the
    form more time to shine (so the player is less likely to just beat
    enemies to death before the meltdown triggers), and also to limit
    how far the form scales - eventually the damage penalty will outweigh
    the upsides.
    
    Beast form is now max skill 6 (was 10), and max slay 5 (was 7).
    This keeps skill scaling similar while limiting how long users will
    want to use their 'starter form'. I might still make further changes
    to this, e.g. replacing slay with an aux.

--- crawl-ref/source/mon-info.cc
+++ crawl-ref/source/mon-info.cc
@@ -178,6 +178,8 @@ static monster_info_flags ench_to_mb(const monster& mons, enchant_type ench)
             return MB_MORE_POISONED;
         else
             return MB_MAX_POISONED;
+    case ENCH_GLOW:
+        return mons.get_ench(ench).degree == 1 ? MB_GLOW_LIGHT : MB_GLOW_HEAVY;
     case ENCH_SHORT_LIVED:
     case ENCH_SLOWLY_DYING:
         if (mons.type == MONS_WITHERED_PLANT)
